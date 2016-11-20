bbcf3ba64c8ecddf8dcdcc8aa127efda2e38de55
Sam Ryan <sam.ryan.uofl@gmail.com>
2016-11-20 16:35:16 +0000

    Removed double printing in php

--- php/index.php
+++ php/index.php
@@ -6,11 +6,8 @@
 </a>
 <p>
 <h2>
-<?php echo 'Dryer status: '; ?>
 <?php
 $status_file_name = "/home/pi/dev/vibration_kookye/python/dryer_status.txt";
-# $myfile = fopen("/home/pi/dev/vibration_kookye/dryer_status.txt", "r") or die("Unable to open file!");
-# echo fread($myfile,filesize("/home/pi/dev/vibration_kookye/dryer_status.txt"));
 echo nl2br(file_get_contents($status_file_name));
 ?>
 </h2>
