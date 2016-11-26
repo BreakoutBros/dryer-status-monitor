ed534dab853d952a13bc038965f5636361973ba2
Sam Ryan <sam.ryan.uofl@gmail.com>
2016-11-26 05:13:13 +0000

    Switched to using Pushover

--- python/vibration_sensor.py
+++ python/vibration_sensor.py
@@ -1,13 +1,10 @@
 import RPi.GPIO as GPIO
-import signal
-import sys
+import time
 import time
 import os
 from datetime import datetime
-import httplib, urllib
-
-# Declared at module scope so cleanup function can use it
-pushover_sender = None
+import httplib
+import urllib
 
 
 class PushoverSender:
@@ -19,38 +16,61 @@ class PushoverSender:
         conn = httplib.HTTPSConnection("api.pushover.net:443")
         post_data = {'user': self.user_key, 'token': self.api_key, 'message': text}
         conn.request("POST", "/1/messages.json",
-          urllib.urlencode(post_data), {"Content-type": "application/x-www-form-urlencoded"})
-        print(conn.getresponse().read())
+                     urllib.urlencode(post_data), {"Content-type": "application/x-www-form-urlencoded"})
+        # print(conn.getresponse().read())
+        print('Notifying: ' + text)
 
 
-def signal_handler(signal, frame):
-    cleanup()
-    sys.exit(0)
+class DryerMonitor:
+    poll_duration_secs = 10
+    poll_hz = 100
 
+    def __init__(self, gpio_bcm, pushover_sender):
+        self.gpio = gpio_bcm
+        self.pushover_sender = pushover_sender
+        self.status = None
 
-def init_gpio_input(bcm_pin):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(bcm_pin, GPIO.IN)
+    def __enter__(self):
+        self.push_startup_notification()
+        self.init_gpio()
+        return self
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.push_close_notification()
+        self.cleanup_gpio()
 
-def poll_gpio(duration_secs, frequency_hz, input_pin):
-    start_time = time.time()
-    end_time = start_time + duration_secs
-    count_high = 0
-    count_low = 0
-    while time.time() < end_time:
-        if GPIO.input(input_pin) == 0:
-            count_low += 1
-        else:
-            count_high += 1
-        time.sleep(1.0 / frequency_hz)
-    return count_low, count_high
+    def update_dryer_status(self):
+        new_status = self._poll_gpio()
+        # Send notification if status changed
+        if new_status != self.status:
+            if new_status:
+                self.pushover_sender.send_notification('Dryer is On')
+            else:
+                self.pushover_sender.send_notification('Dryer is Off')
+            self.status = new_status
 
+    def init_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio, GPIO.IN)
 
-def cleanup():
-    GPIO.cleanup()
-    if pushover_sender is not None:
-        pushover_sender.send_notification('Dryer monitor stopped')
+    def cleanup_gpio(self):
+        GPIO.cleanup()
+
+    def push_startup_notification(self):
+        self.pushover_sender.send_notification('Dryer Monitor Started')
+
+    def push_close_notification(self):
+        self.pushover_sender.send_notification('Dryer Monitor Stopped')
+
+    def _poll_gpio(self):
+        start_time = time.time()
+        end_time = start_time + DryerMonitor.poll_duration_secs
+        count_high = 0
+        while time.time() < end_time:
+            if GPIO.input(self.gpio) != 0:
+                count_high += 1
+            time.sleep(1.0 / DryerMonitor.poll_hz)
+        return count_high > 0
 
 
 def get_key(filename):
@@ -59,55 +79,16 @@ def get_key(filename):
     return key
 
 
-
-def main_loop():
-    # Setup GPIO
-    vib_pin = 21
-    init_gpio_input(vib_pin)
-
-    # Construct file names using relative path
-    status_file = os.path.join(os.path.dirname(__file__), 'dryer_status.txt')
-    user_key_file = os.path.join(os.path.dirname(__file__), 'user.key')
-    api_key_file = os.path.join(os.path.dirname(__file__), 'apitoken.key')
-
+def main():
     # Get Pushover keys from files
-    user_key = get_key(user_key_file)
-    api_key = get_key(api_key_file)
+    user_key = get_key(os.path.join(os.path.dirname(__file__), 'user.key'))
+    api_key = get_key(os.path.join(os.path.dirname(__file__), 'apitoken.key'))
 
     pushover_sender = PushoverSender(user_key, api_key)
-    pushover_sender.send_notification('Dryer Monitor Started')
-
-    last_status = None
-    while True:
-        low, high = poll_gpio(10, 100, vib_pin)
-        status = 'Dryer Status: '
-        if high > 1:
-            new_status = True
-            status += 'On\n'
-        else:
-            new_status = False
-            status += 'Off\n'
-
-        if last_status != new_status
-            if new_status:
-                pushover_sender.send_notification('Dryer is running')
-            else:
-                pushover_sender.send_notification('Dryer is off')
-            last_status = new_status
-
-        with open(status_file, 'w') as f:
-            status += 'Updated: ' + str(datetime.now())
-            f.write(status)
-
-
-def main():
-    try:
-        # Catch signals to do GPIO cleanup and push notification before exiting
-        signal.signal(signal.SIGINT, signal_handler)
-        main_loop()
-    finally:
-        cleanup()
-
+    with DryerMonitor(21, pushover_sender) as dm:
+        dm.update_dryer_status()
+        while True:
+            dm.update_dryer_status()
 
 if __name__ == '__main__':
     main()
